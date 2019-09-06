a9d845f7eea1cfa76564649af5780f27d5cd66b4
videosgames <54912894+videosgames@users.noreply.github.com>
2019-09-06 14:11:54 -0600

    adding colour to first project

--- firstproject/sketch.js
+++ firstproject/sketch.js
@@ -8,10 +8,29 @@
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(0);
 }
-
+let theColour = 1
+let redAmount = 255;
+let greenAmount = 0;
+let changey = 1;
 function draw() {
-  background(220);
-  FileList(0);
-  ellipse(mouseX, mouseY, 100, 100);
+  if (mouseIsPressed) {
+    fill(redAmount, greenAmount, 0);
+    noStroke();
+    ellipse(mouseX, mouseY, 25, 25);
+
+    if (theColour === 1) {
+      greenAmount += changey;
+    }
+    if (greenAmount > 255) {
+      changey *= -1
+    }
+  }
+  if (keyIsPressed === true) {
+    if (keyCode === BACKSPACE) {
+      background(0);
+    } 
+  }
+  console.log(greenAmount);
 }

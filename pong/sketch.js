3e40004a092de3603eab8da73ca030de529d3684
videosgames <54912894+videosgames@users.noreply.github.com>
2019-09-10 13:10:34 -0600

    Added link "Ping Pong"

---
+++ pong/sketch.js
@@ -0,0 +1,16 @@
+// Ping Pong
+// Morgan Rusk
+// September 10, 2019
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+}
+
+function draw() {
+  
+}
